75028216e5d816cc55c9421ea30d9f82d2da42d6
felix <feliquisds@gmail.com>
2024-01-14 21:06:05 -0300

    fix: aprimora validação de padrão de placa

--- DesafioFundamentos/Models/Estacionamento.cs
+++ DesafioFundamentos/Models/Estacionamento.cs
@@ -17,27 +17,57 @@ namespace DesafioFundamentos.Models
         {
             Console.Write("Digite a placa do veículo para estacionar: ");
             string placa = Console.ReadLine();
+            int placaModelo = VerificarPlaca(placa);
 
-            bool ok = Regex.IsMatch(placa, "^[a-zA-Z0-9-]+$");
-
-            if (veiculos.Exists(x => x.ToUpper() == placa.ToUpper())) Console.WriteLine("Esse veículo já foi cadastrado.");
-            else if (!ok || placa.Length < 7 || placa.Length > 8) Console.WriteLine("Placa inválida. Confirme que a placa esteja no padrão Mercosul.");
+            if (placaModelo == 0) Console.WriteLine("Placa inválida. Confirme que a placa esteja no padrão Mercosul.");
+            else if (veiculos.Exists(x => x.ToUpper() == placa.ToUpper())) Console.WriteLine("Esse veículo já foi cadastrado.");
             else
             {
                 veiculos.Add(placa.ToUpper());
+                Console.WriteLine("Padrão de placa detectado: " + (placaModelo == 1 ? "Mercosul antigo" : "Mercosul novo"));
                 Console.WriteLine("O veículo foi cadastrado.");
             }
         }
 
+        // confirma se placa é válida e descobre padrão utilizado
+        // retorna 0 = inválido
+        // retorna 1 = padrão mercosul antigo (XXX0000)
+        // retorna 2 = padrão mercosul novo (XXX0X00)
+        int VerificarPlaca(string placa)
+        {
+            if (placa.Length != 7) return 0;
+
+            // primeiros três caracteres são somente letras?
+            bool verifica1 = Regex.IsMatch(placa.Substring(0, 3), "^[a-zA-Z]+$");
+
+            // últimos quatro caracteres são somente números?
+            bool verifica2 = Regex.IsMatch(placa.Substring(3), "^[0-9]+$");
+
+            // últimos quatro caracteres seguem a ordem do mercosul novo?
+            bool verifica3 = Regex.IsMatch(placa.Substring(3, 1), "^[0-9]+$") &&
+                          Regex.IsMatch(placa.Substring(4, 1), "^[a-zA-Z]+$") &&
+                          Regex.IsMatch(placa.Substring(5), "^[0-9]+$");
+
+            if (verifica1 && verifica2) return 1;
+            if (verifica1 && verifica3) return 2;
+
+            return 0;
+        }
+
         public void RemoverVeiculo()
         {
-            if (veiculos.Any())
+            if (!veiculos.Any()) Console.WriteLine("Não há veículos estacionados.");
+            else
             {
                 Console.Write("Digite a placa do veículo para remover: ");
                 string placa = Console.ReadLine();
 
-                // Verifica se o veículo existe
-                if (veiculos.Exists(x => x.ToUpper() == placa.ToUpper()))
+                // verifica se o veículo não existe
+                if (!veiculos.Exists(x => x.ToUpper() == placa.ToUpper()))
+                {
+                    Console.WriteLine("Desculpe, esse veículo não está estacionado aqui. Confira se digitou a placa corretamente.");
+                }
+                else
                 {
                     Console.Write("Digite a quantidade de horas que o veículo permaneceu estacionado: ");
                     decimal horas = 0;
@@ -59,20 +89,18 @@ namespace DesafioFundamentos.Models
                         Console.WriteLine("Um errou ocorreu. Tenha certeza que digitou um número válido.");
                     }
                 }
-                else Console.WriteLine("Desculpe, esse veículo não está estacionado aqui. Confira se digitou a placa corretamente.");
             }
-            else Console.WriteLine("Não há veículos estacionados.");
         }
 
         public void ListarVeiculos()
         {
-            // Verifica se há veículos no estacionamento
-            if (veiculos.Any())
+            // verifica se não há veículos no estacionamento
+            if (!veiculos.Any()) Console.WriteLine("Não há veículos estacionados.");
+            else
             {
                 Console.WriteLine("Os veículos estacionados são:");
                 foreach (string placa in veiculos) Console.WriteLine(placa);
             }
-            else Console.WriteLine("Não há veículos estacionados.");
         }
     }
 }
